7ba2f2933c1a592d7e403cf95e84838adddb4680
Darren322 <xiia0_darren@hotmail.com>
2026-08-28 18:36:08 +0800

    chore: removed

--- apps/server/dist/runner-factory.js
+++
@@ -1,7 +0,0 @@
-import { ContainerCodexRunner } from "./container-codex-runner.js";
-import { CodexRunner } from "./codex-runner.js";
-export function createRunner(config) {
-    return config.runtimeProvider === "container"
-        ? new ContainerCodexRunner(config)
-        : new CodexRunner(config);
-}
